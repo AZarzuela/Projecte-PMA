71f6c5889113549d6f31ddb7e3dfdcacddc15845
Adrià Zarzuela <adri_zarzu@hotmail.com>
2016-10-25 22:41:26 +0200

    Implementació setResult() i operatorClick()

--- Calculadora/app/src/main/java/com/edu/upc/pma/calculadora/CalculadoraActivity.java
+++ Calculadora/app/src/main/java/com/edu/upc/pma/calculadora/CalculadoraActivity.java
@@ -3,7 +3,6 @@ package com.edu.upc.pma.calculadora;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
 
@@ -11,6 +10,10 @@ import android.view.View;
 public class CalculadoraActivity extends AppCompatActivity {
 
     TextView text ;
+    String signe;
+    String num1;
+    String num2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +38,60 @@ public class CalculadoraActivity extends AppCompatActivity {
         text.setText(text.getText()+ numero);
     }
 
+    public void operatorClick(View view) {
+        Button b = (Button) view;
+        // Guardar Operador en una variable
+        CharSequence operator = b.getText();
+        signe = operator.toString();
 
+        // Guardar numero en una variable
+        CharSequence numero1 = text.getText();
+        num1 = numero1.toString();
+
+        // Clear del TextView
+        text.setText("");
+    }
+
+
+    public void setResult(View view) {
+        // Guardo el nou numero introduit en una variable
+        CharSequence numero2 = text.getText();
+        num2 = numero2.toString();
+
+        Float value1 = Float.parseFloat(num1);
+        Float value2 = Float.parseFloat(num2);
+
+        // Switch per saber quina operació hem de fer
+        switch (signe) {
+            case "+" :
+                Float sum = value1+value2;
+                String num1 = sum.toString();
+                text.setText(num1);
+                /*if (Math.round(result) == result){
+                    Integer res = Integer.parseInt(num);
+                    text.setText(res);
+                } else {
+                    text.setText(num);
+                } */
+                break;
+            case "-":
+                Float rest = value1-value2;
+                String num2 = rest.toString();
+                text.setText(num2);
+            break;
+
+            case "/":
+                Float div = value1/value2;
+                String num3 = div.toString();
+                text.setText(num3);
+            break;
+
+            case "x":
+                Float mult = value1*value2;
+                String num4 = mult.toString();
+                text.setText(num4);
+            break;
+
+        }
+    }
 }
