cad939532c5a9f387e04e64b5933cc1aa97ee940
Felipe C.Cuzcano <christiancuz@gmail.com>
2016-10-26 18:40:41 +0200

    Actualización salida TextView int o float

--- Calculadora/app/src/main/java/com/edu/upc/pma/calculadora/CalculadoraActivity.java
+++ Calculadora/app/src/main/java/com/edu/upc/pma/calculadora/CalculadoraActivity.java
@@ -52,7 +52,6 @@ public class CalculadoraActivity extends AppCompatActivity {
         text.setText("");
     }
 
-
     public void setResult(View view) {
         // Guardo el nou numero introduit en una variable
         CharSequence numero2 = text.getText();
@@ -63,33 +62,57 @@ public class CalculadoraActivity extends AppCompatActivity {
 
         // Switch per saber quina operació hem de fer
         switch (signe) {
+
             case "+" :
                 Float sum = value1+value2;
-                String num1 = sum.toString();
-                text.setText(num1);
-                /*if (Math.round(result) == result){
-                    Integer res = Integer.parseInt(num);
-                    text.setText(res);
-                } else {
-                    text.setText(num);
-                } */
+                if ( sum % 1 == 0){
+                    int d = (int) Math.floor(sum);
+                    String num_1 = Integer.toString(d);
+                    text.setText(num_1);
+                }
+                else {
+                    String num1 =sum.toString();
+                    text.setText(num1);
+                }
                 break;
+
             case "-":
                 Float rest = value1-value2;
-                String num2 = rest.toString();
-                text.setText(num2);
+                if ( rest % 1 == 0){
+                    int d = (int) Math.floor(rest);
+                    String num_2 = Integer.toString(d);
+                    text.setText(num_2);
+                }
+                else {
+                    String num2 =rest.toString();
+                    text.setText(num2);
+                }
             break;
 
             case "/":
                 Float div = value1/value2;
-                String num3 = div.toString();
-                text.setText(num3);
+                if ( div % 1 == 0){
+                    int d = (int) Math.floor(div);
+                    String num_3 = Integer.toString(d);
+                    text.setText(num_3);
+                }
+                else {
+                    String num3 =div.toString();
+                    text.setText(num3);
+                }
             break;
 
             case "x":
                 Float mult = value1*value2;
-                String num4 = mult.toString();
-                text.setText(num4);
+                if ( mult % 1 == 0){
+                    int d = (int) Math.floor(mult);
+                    String num_4 = Integer.toString(d);
+                    text.setText(num_4);
+                }
+                else {
+                    String num4 =mult.toString();
+                    text.setText(num4);
+                }
             break;
 
         }
