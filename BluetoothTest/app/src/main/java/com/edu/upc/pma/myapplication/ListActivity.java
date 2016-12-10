fce0c4d25ea54c48ad06edace17a029287c2ada3
Adrià Zarzuela <adri_zarzu@hotmail.com>
2016-12-10 19:39:28 +0100

    Afegeixo prova de quadre Dialeg per conectarse a un dispositiu

--- BluetoothTest/app/src/main/java/com/edu/upc/pma/myapplication/ListActivity.java
+++ BluetoothTest/app/src/main/java/com/edu/upc/pma/myapplication/ListActivity.java
@@ -4,11 +4,15 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -20,6 +24,9 @@ public class ListActivity extends AppCompatActivity {
     // Declarem referencies
     private ListView listView;
 
+    //Adaptador de la Arraylist
+    ArrayAdapter<String> adapter;
+
     // Array list perqué mostrarem el nom i la direcció MAC
     private ArrayList<String> mDeviceList = new ArrayList<String>();
     private BluetoothAdapter mBluetoothAdapter;
@@ -29,8 +36,11 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        // Declarem la list view
+        // Declarem la list view i el seu adaptador
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDeviceList);
         listView = (ListView) findViewById(R.id.listView);
+
+        listView.setAdapter(adapter);
 
         // Declarem el Bluetooth Adapter
         // https://developer.android.com/reference/android/bluetooth/BluetoothAdapter.html
@@ -39,6 +49,39 @@ public class ListActivity extends AppCompatActivity {
 
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(mReceiver, filter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, final int pos, long id) {
+                // Creacio quadre de dialeg per conectar amb un dispositiu
+                String dialog =
+                        String.format("Are you sure you want to connect with: '%s' ?");
+                // Constructor del quadre de dialeg
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this);
+
+                // Evitar que l'usuari pugui sortir del quadre de dialeg sense apretar un dels dos botons
+                builder.setCancelable(false);
+
+                // Mostrar el quadre de dialeg
+                builder.setMessage(dialog);
+
+                //Creació del primer 'button' amb un recurs d'string
+                // DE MOMENT NO FARA RES
+                builder.setPositiveButton(R.string.conectar, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Aquí aniria el codi de conexio amb el dispositiu clicat
+                        //...
+
+                    }
+                });
+
+                //Creació del segon 'button' amb un recurs d'string
+                builder.setNegativeButton(R.string.calcelar, null);
+
+                builder.create().show();
+            }
+        });
 
     }
 
@@ -61,7 +104,7 @@ public class ListActivity extends AppCompatActivity {
                 listView.setAdapter(new ArrayAdapter<String>(context,
                         android.R.layout.simple_list_item_1, mDeviceList));
                 if (device.getAddress().equals(id)){
-                    Toast.makeText(ListActivity.this, "Esta MAC esta cerca", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ListActivity.this, "MAC detected", Toast.LENGTH_SHORT).show();
                 }
             }
         }
