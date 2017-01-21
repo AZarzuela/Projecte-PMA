2787f02d90b0e3ae314838bb730c179fa89af6d6
Adrià Zarzuela <adri_zarzu@hotmail.com>
2017-01-21 18:50:13 +0100

    Afegeixo classe DeviceItem

---
+++ BluetoothTest/app/src/main/java/com/edu/upc/pma/myapplication/DeviceItem.java
@@ -0,0 +1,30 @@
+package com.edu.upc.pma.myapplication;
+
+public class DeviceItem {
+    private String deviceName;
+    private String address;
+    private int connected;
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public int getConnected() {
+        return connected;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public DeviceItem(String name, String address, int connected){
+        this.deviceName = name;
+        this.address = address;
+        this.connected = connected;
+    }
+}
+
