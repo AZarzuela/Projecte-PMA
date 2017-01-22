f33639d58cd2e8b9b24f0a42c251a977d2a951b8
victorinho <vic>
2017-01-22 19:54:52 +0100

    amatch parent logo

--- BluetoothTest/app/src/main/java/com/edu/upc/pma/myapplication/ListActivity.java
+++ BluetoothTest/app/src/main/java/com/edu/upc/pma/myapplication/ListActivity.java
@@ -1,16 +1,11 @@
 package com.edu.upc.pma.myapplication;
 
-import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -19,13 +14,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-import android.os.Handler;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.UUID;
 
 
 public class ListActivity extends AppCompatActivity {
